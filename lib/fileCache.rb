7d3ffbeb3a1e4ddb1537e1ea0952e44e6fa021b8
gong023 <gon.gong.gone@gmail.com>
2014-07-12 20:59:10 +0900

    naive implementation

--- lib/fileCache.rb
+++ lib/fileCache.rb
@@ -1,5 +1,37 @@
 require "fileCache/version"
 
-module FileCache
-  # Your code goes here...
+class FileCache
+  def initialize(path = '')
+    if ! path.empty?
+      @file_path = path
+    elsif ! ENV['FILECACHE_PATH'].nil? && ! ENV['FILECACHE_PATH'].empty?
+      @file_path = ENV['FILECACHE_PATH']
+    else
+      @file_path = '/tmp'
+    end
+  end
+
+  def set(key, value)
+    FileUtils.mkdir_p("#{@file_path}")
+    File.write("#{@file_path}/#{key}", value)
+  end
+
+  def get key
+    has?(key) ? File.read("#{@file_path}/#{key}") : nil
+  end
+
+  def has? key
+    File.exists?("#{@file_path}/#{key}")
+  end
+
+  def delete key
+    File.delete("#{@file_path}/#{key}") if has? key
+  end
+
+  def flush
+    Dir.entries(@file_path) do |item|
+      next if item == '.' || item == '..'
+      delete item
+    end
+  end
 end
--- lib/fileCache/version.rb
+++ lib/fileCache/version.rb
@@ -1,3 +1,3 @@
-module FileCache
+class FileCache
   VERSION = "0.0.1"
 end
